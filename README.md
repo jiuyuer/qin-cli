859611f17536fc1a42c84ad833ead6bfb7559e93
jiuyuer <jiuyue14@163.com>
2022-04-21 16:08:11 +0800

    调整模板测试

--- README.md
+++ README.md
@@ -1,50 +1,32 @@
-<h2 align="center">js-plugin-cli</h2>
+<h2 align="center">qin-cli</h2>
 
 <p align="center">A lightweight javascript plugin CLI.</p>
 
-<p align="center">
-<img src="https://img.shields.io/badge/build-passing-brightgreen?style=flat-square" alt="Build Status">
-<img src="https://img.shields.io/github/package-json/v/zpfz/js-plugin-cli?style=flat-square&color=orange" alt="Version">
-<img src="https://img.shields.io/badge/license-MIT-brightgreen?style=flat-square&color=blue" alt="MIT">
-<img alt="npm" src="https://img.shields.io/npm/dt/js-plugin-cli?style=flat-square&color=red" alt="downloads">
-</p>
-
 # Installation
 ```
-$ npm i js-plugin-cli -g
+$ npm i qin-cli -g
 ```
 # Usage
 Run the following command line to create the project:
 ```
-$ js-plugin-cli init myproject
+$ qin-cli init myproject
 ```
 
 # Parameter
 ## init <PROJECT_NAME>
 Create the JavaScript plugin project:
 ```
-$ js-plugin-cli init myproject
+$ qin-cli init myproject
 ```
 
 ## upgrade
 Check the new version is available or not:
 ```
-$ js-plugin-cli upgrade
+$ qin-cli upgrade
 ```
 
 ## template
 You can download or upgrade the template from mirror:
 ```
-$ js-plugin-cli template
+$ qin-cli template
 ```
-
-## mirror <TEMPLATE_MIRROR>
-You can also set the template mirror like this:
-```
-$ js-plugin-cli mirror https://zpfz.vercel.app/download/files/frontend/tpl/js-plugin-cli/
-```
-**NOTE**  
-You can customize the template mirror link by youself, but the template file name must be `template.zip`, and the mirror link should be `/` ending.  
-For example, the full link to your custom template mirror is `https://example.com/mirror/template.zip`, the mirror link that js-plugin-cli can recognize should be `https://example.com/mirror/`.  
-
-You can download the js-plugin-cli template from [js-plugin-cli/tpl](https://github.com/zpfz/js-plugin-cli/tree/master/tpl). 
--- bin/index.js
+++ bin/index.js
@@ -1,47 +1,47 @@
 #!/usr/bin/env node
 
-const program = require('commander')
+const program = require('commander');
 
-const updateChk = require('../lib/update')
-const setMirror = require('../lib/mirror')
-const dlTemplate = require('../lib/download')
-const initProject = require('../lib/init')
+const updateChk = require('../lib/update');
+const setMirror = require('../lib/mirror');
+const dlTemplate = require('../lib/download');
+const initProject = require('../lib/init');
 
 // version
-program.version(require('../package.json').version, '-v, --version')
+program.version(require('../package.json').version, '-v, --version');
 
 // upgrade
 program
-	.command('upgrade')
-	.description('Check the js-plugin-cli version.')
-	.action(() => {
-		updateChk()
-	})
+  .command('upgrade')
+  .description('Check the qin-cli version.')
+  .action(() => {
+    updateChk();
+  });
 
 // mirror
 program
-	.command('mirror <template_mirror>')
-	.description('Set the template mirror.')
-	.action((tplMirror) => {
-		setMirror(tplMirror)
-	})
+  .command('mirror <template_mirror>')
+  .description('Set the template mirror.')
+  .action(tplMirror => {
+    setMirror(tplMirror);
+  });
 
 // template
 program
-	.command('template')
-	.description('Download template from mirror.')
-	.action(() => {
-		dlTemplate()
-	})
+  .command('template')
+  .description('Download template from mirror.')
+  .action(() => {
+    dlTemplate();
+  });
 
 // init
 program
-	.name('js-plugin-cli')
-	.usage('<commands> [options]')
-	.command('init <project_name>')
-	.description('Create a javascript plugin project.')
-	.action(project => {
-		initProject(project)
-	})
+  .name('qin-cli')
+  .usage('<commands> [options]')
+  .command('init <project_name>')
+  .description('Create a javascript plugin project.')
+  .action(project => {
+    initProject(project);
+  });
 
-program.parse(process.argv)
+program.parse(process.argv);
